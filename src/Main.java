1acaa3b5c86572dc47e9ca5b45fac6956d0bd476
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-01-08 13:32:10 -0500

    intialization period

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
+
+import java.util.*;
 import java.util.List;
-import java.util.Random;
 
 import agents.*;
 import definitions.Citizenship;
@@ -17,11 +17,16 @@ public class Main
     private static List<Worker> workers;
     private static Auctioneer auctioneer;
     private static FirmStats statistics_firms;
+    private static int setup_workers;
+    private static int setup_firms;
+    private static int setup_period;
+    private static int num_firms;
+    private static Random seed_generator;
 
     public static void initialisation()
     {
 
-        final int num_firms = 100;
+        num_firms = 100;
         final double sauditization_percentage = 0;
         final int num_saudis = 3800;
         final int num_expats = 7000;
@@ -30,7 +35,11 @@ public class Main
         final double productivity_mean_expat = 6854.24 / 30;
         final double wage_mean_expat = 764.77 / 30;
 
-        Random seed_generator = new Random();
+        setup_period = 500;
+        setup_workers = (int) Math.ceil((double)(num_expats + num_saudis) / setup_period);
+        setup_firms = (int) Math.ceil((double) num_firms / setup_period);
+
+        seed_generator = new Random();
 
         statistics_firms = new FirmStats(num_firms);
 
@@ -42,6 +51,7 @@ public class Main
         workers = new ArrayList<Worker>();
 
         Random rnd = new Random(seed_generator.nextLong());
+
         for (int i = 0; i < num_saudis; i++)
         {
             workers.add(
@@ -64,13 +74,17 @@ public class Main
                      )
             );
         }
+        Collections.shuffle(workers);
 
         apply_to_firm = new ArrayList<List<Worker>>();
 
         firms = new ArrayList<Firm>();
 
+    }
 
-        for (int i = 0; i < num_firms; i++)
+    private static void create_firms(int number)
+    {
+        for (int i = 0; i < number; i++)
         {
             ArrayList<Worker> applications = new ArrayList<Worker>();
             apply_to_firm.add(applications);
@@ -89,8 +103,12 @@ public class Main
 
     public static void run()
     {
-        for (int day = 0; day < 1000; day++)
+        for (int day = 0; day < 2000; day++)
         {
+            if (day < setup_period)
+            {
+                create_firms(Math.min(setup_firms, num_firms - firms.size()));
+            }
             for (Firm firm: firms)
             {
                 firm.set_prices_demand();
@@ -101,13 +119,19 @@ public class Main
             {
                 firm.advertise();
             }
+            int i = 0;
             for (Worker worker: workers)
             {
                 worker.apply();
+                i++;
+                if (i > setup_workers * day)
+                {
+                    break;
+                }
             }
             for (Firm firm: firms)
             {
-                firm.hire();
+                firm.hiring();
             }
             for (Firm firm: firms)
             {
@@ -132,17 +156,28 @@ public class Main
             }
             for (Firm firm: firms)
             {
-                firm.fire();
+                firm.firing();
+            }
+            for (int h = firms.size()-1; h >= 0; h--) {
+                if (firms.get(h).out_of_business())
+                {
+                    firms.remove(h);
+                }
             }
             if (day % 10 == 0)
             {
                 System.out.print(day);
-                System.out.print(", ");
+                System.out.print("\t");
                 updateFirmStatistics();
                 statistics_firms.printcsv();
                 System.out.println("");
             }
+            if (day == 1500)
+            {
+                auctioneer.income *= 10;
+            }
         }
+        WorkerStatistics.net_contribution(workers, auctioneer.market_price);
     }
 
     private static void updateFirmStatistics() {
