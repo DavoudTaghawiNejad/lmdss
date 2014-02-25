385702354630828044dee4de5cc4fbc50d475316
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-02-25 14:47:42 -0500

    firm creation now in initization

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,42 +1,42 @@
-import java.io.IOException;
 import java.util.*;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import agents.*;
-import com.sun.java.swing.plaf.gtk.resources.gtk_sv;
 import definitions.Citizenship;
 import definitions.WorkerStatistics;
 import tools.DBConnection;
 import tools.WorkerRecord;
 
+import static java.lang.Integer.min;
+
 public class Simulation
 {
-    private static double sauditization_percentage;
-    private static List<Worker> workers;
-    private static Auctioneer auctioneer;
-    private static int setup_workers;
-    private static int setup_firms;
-    private static int setup_period;
-    private static int num_firms;
-    private static Random seed_generator;
-    private static int simulation_length;
-    private static int policy_change_time;
-	private static List<List<WorkerRecord>> apply_to_firm;
-	private static List<Firm> firms;
-	private static Newspaper newspaper_saudi;
-	private static Newspaper newspaper_expat;
-    private static AtomicInteger day = new AtomicInteger();
-    private static double wage_std;
-    private static double reservation_wage_saudi;
-    private static double reservation_wage_expat;
-    private static DBConnection db_connection;
+    private final List<Worker> workers;
+    private final Auctioneer auctioneer;
+    private final int setup_workers;
+    private final int setup_firms;
+    private final int setup_period;
+    private final int num_firms;
+    private final Random seed_generator;
+    private final int simulation_length;
+    private final int policy_change_time;
+	private final List<List<WorkerRecord>> apply_to_firm;
+	private final List<Firm> firms;
+	private final Newspaper newspaper_saudi;
+	private final Newspaper newspaper_expat;
+    private final AtomicInteger day = new AtomicInteger();
+    private final double wage_std;
+    private final double reservation_wage_saudi;
+    private final double reservation_wage_expat;
+    private final DBConnection db_connection;
+    private final double initial_sauditization_percentage;
+    private final ArrayList<Firm> firms_reserve = new ArrayList<Firm>();
 
 
     public Simulation()
     {
         num_firms = 100;
-        final double sauditization_percentage = 0;
         final int num_saudis = 3800;
         final int num_expats = 7000;
         final double productivity_mean_saudi = 6854.24 / 30;
@@ -50,50 +50,60 @@ public class Simulation
         final double reapplication_probability = 0.03 / 356;
         final double saudi_tax_percentage = 0;
         final double saudi_tax_per_head = 0;
+        initial_sauditization_percentage = 0;
+        final double sector_spending = 1000000000;
+        final double love_for_variety = 0.5;
 
         setup_period = 500;
         simulation_length = 2000;
         policy_change_time = 1500;
         wage_std = 3137.39 / 30;
+
+        long seed = 5302877246224082029L;
+
+
+        System.out.println(seed);
         setup_workers = (int) Math.ceil((double)(num_expats + num_saudis) / setup_period);
         setup_firms = (int) Math.ceil((double) num_firms / setup_period);
 
-        //final long seed = (new Random().nextLong());
-        final long seed = 5302877246224082029L;
-        System.out.println(seed);
+        if (seed == 0L)
+        {
+            seed = (new Random().nextLong());
+        }
         seed_generator = new Random(seed);
+        Random rnd = new Random(seed_generator.nextLong());
         db_connection = new DBConnection(seed);
 
-        auctioneer = new Auctioneer(0.5, 1000000000);
+        auctioneer = new Auctioneer(love_for_variety, sector_spending);
 
         newspaper_saudi = new Newspaper(seed_generator.nextLong());
         newspaper_expat = new Newspaper(seed_generator.nextLong());
 
         workers = new ArrayList<Worker>();
 
-        Random rnd = new Random(seed_generator.nextLong());
 
         create_workers(num_saudis, num_expats, productivity_mean_saudi, productivity_mean_expat, expat_minimum_wage,
                 saudi_minimum_wage, saudi_tax_percentage, expat_tax_percentage, saudi_tax_per_head, expat_tax_per_head,
                 reapplication_probability, 0, reservation_wage_saudi, reservation_wage_expat, rnd
         );
-        Collections.shuffle(workers, new Random(seed_generator.nextLong()));
 
         apply_to_firm = new ArrayList<List<WorkerRecord>>();
-
         firms = new ArrayList<Firm>();
+        create_firms(num_firms, initial_sauditization_percentage);
     }
 
-    private static void create_workers(double num_saudis, double num_expats, double productivity_mean_saudi, double productivity_mean_expat,
+    private final void create_workers(double num_saudis, double num_expats, double productivity_mean_saudi, double productivity_mean_expat,
                                        double expat_minimum_wage, double saudi_minimum_wage, double saudi_tax_percentage, double expat_tax_percentage,
-                                       double saudi_tax_per_head, double expat_tax_per_head, double reapplication_probability_saudi, double reapplication_probability_expat, double reservation_wage_saudi, double reservation_wage_expat, Random rnd)
+                                       double saudi_tax_per_head, double expat_tax_per_head, double reapplication_probability_saudi,
+                                       double reapplication_probability_expat, double reservation_wage_saudi, double reservation_wage_expat, Random rnd
+    )
     {
         _create_workers(Citizenship.SAUDI, num_saudis, reservation_wage_saudi, productivity_mean_saudi, saudi_minimum_wage, saudi_tax_percentage, saudi_tax_per_head, reapplication_probability_saudi, rnd);
         _create_workers(Citizenship.EXPAT, num_expats, reservation_wage_expat, productivity_mean_expat, expat_minimum_wage, expat_tax_percentage, expat_tax_per_head, reapplication_probability_expat, rnd);
-
+        Collections.shuffle(workers, new Random(seed_generator.nextLong()));
     }
 
-    private static void _create_workers(Citizenship citizenship, double number, double reservation_wage, double productivity_mean, double minimum_wage, double tax_percentage, double tax_per_head,
+    private final void _create_workers(Citizenship citizenship, double number, double reservation_wage, double productivity_mean, double minimum_wage, double tax_percentage, double tax_per_head,
                                         double reapplication_probability, Random rnd)
     {
         for (int i = 0; i < number; i++)
@@ -115,14 +125,14 @@ public class Simulation
         }
     }
 
-    private static void create_firms(int number)
+    private final void create_firms(int number, double initial_sauditization_percentage)
     {
-        final int last_id = firms.size();
+        final int last_id = 0;
         for (int i = 0; i < number; i++)
         {
             ArrayList<WorkerRecord> applications = new ArrayList<WorkerRecord>();
             apply_to_firm.add(applications);
-            firms.add(
+            firms_reserve.add(
                     new Firm(
                             last_id + i,
                             seed_generator.nextLong(),
@@ -130,7 +140,7 @@ public class Simulation
                             newspaper_saudi,
                             newspaper_expat,
                             auctioneer,
-                            sauditization_percentage,
+                            initial_sauditization_percentage,
                             day,
                             wage_std
                     )
@@ -138,7 +148,7 @@ public class Simulation
         }
     }
 
-    public static void run() {
+    public final void run() {
 
         for (int iday = 0; iday < simulation_length; iday++)
         {
@@ -146,7 +156,8 @@ public class Simulation
             auctioneer.new_round();
             if (iday < setup_period)
             {
-                create_firms(Math.min(setup_firms, num_firms - firms.size()));
+                firms.addAll(firms_reserve.subList(0, min(setup_firms, firms_reserve.size())));
+                firms_reserve.subList(0, min(setup_firms, firms_reserve.size())).clear();
             }
             newspaper_saudi.clear_job_ads();
             newspaper_expat.clear_job_ads();
@@ -222,7 +233,7 @@ public class Simulation
         db_connection.close();
     }
 
-    private static void statistics(int iday) {
+    private final void statistics(int iday) {
 
         if (
             iday >= 500
