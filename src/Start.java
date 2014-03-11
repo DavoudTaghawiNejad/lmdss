520b23c001a789c79faeed967d07bbd481443f34
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-03-10 21:47:53 -0400

    help and alternative/optional parameters

--- src/Start.java
+++ src/Start.java
@@ -1,29 +1,50 @@
+import agents.CalibrationStatistics;
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class Start
 {
-    public static void main(String [] args)
+    public static void main(String [] raw_args)
     {
-        assert args.length <= 1: "should only contain one json parameter set";
+        List<String> args = Arrays.asList(raw_args);
+        if (args.contains("--help") || args.contains("-h") || args.size() == 1 || args.size() > 3)
+        {
+            System.out.println("start t  for time series");
+            System.out.println("start p  for panel data");
+            System.out.println("start tp  for both");
+            System.out.println("start tp {Parameters} for both with parameters");
+            System.out.println("start c {Parameters} for calibration");
+
+            System.out.println("Parameters:");
+            System.out.println();
+            System.out.println(new Parameters().toString());
+            System.out.println();
+            System.out.println("Results");
+            System.out.println(new CalibrationStatistics().json().toString());
+            System.out.println();
+            System.out.println("All parameters and results are monthly");
+            return;
+        }
         delete_database();
         long started = System.currentTimeMillis();
         Simulation simulation;
-        if (args.length == 0)
+        if (args.size() == 0)
         {
             simulation = new Simulation();
         }
         else
         {
-            simulation = new Simulation(args[0]);
+            simulation = new Simulation(args.get(0), args.get(1));
         }
         JSONObject simulation_output = simulation.run();
         dump_csv();
         simulation_output.put("run time", (System.currentTimeMillis() - started) / 1000.0);
-        System.out.println();
-        System.out.println(simulation_output);
+        simulation_output.put("hash", simulation.getSha());
+        System.out.print(simulation_output);
     }
 
     private static void delete_database()
