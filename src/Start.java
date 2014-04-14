1a4c8d06d95c2a9b02de7c86144b5c07792f01d9
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-04-14 18:00:32 -0400

    json from commandline angument or file

--- src/Start.java
+++ src/Start.java
@@ -1,24 +1,26 @@
 import agents.CalibrationStatistics;
-import org.json.simple.JSONObject;
 
-import java.io.IOException;
+import org.json.simple.JSONObject;
+import java.io.FileReader;
+import org.json.simple.parser.JSONParser;
 import java.util.Arrays;
 import java.util.List;
 
 
 public class Start
 {
-    public static void main(String [] raw_args) throws Exception
+    public static void main(String[] raw_args) throws Exception
     {
         List<String> args = Arrays.asList(raw_args);
-        if (args.contains("--help") || args.contains("-h") || args.size() <2 || args.size() > 3)
+        if (args.contains("--help") || args.contains("-h") || args.size() < 2 || args.size() > 3)
         {
             System.out.println("start t  for time series");
             System.out.println("start p  for panel data");
             System.out.println("start tp  for both");
             System.out.println("start tp {Assumptions} for both with parameters");
             System.out.println("start c {Assumptions} for calibration");
-
+            System.out.println("start c filename.json for calibration");
+            System.out.println();
             System.out.println("Parameters:");
             System.out.println("{");
             System.out.println("\"assumptions\":");
@@ -34,45 +36,24 @@ public class Start
             System.out.println("All parameters and results are monthly");
             return;
         }
-        delete_database();
         long started = System.currentTimeMillis();
         Simulation simulation;
+        String parameters;
+        if (args.get(1).startsWith("{"))
         {
-            simulation = new Simulation(args.get(0), args.get(1));
+            parameters = args.get(1);
         }
+        else
+        {
+            JSONParser parser = new JSONParser();
+            parameters = parser.parse(new FileReader(args.get(1))).toString();
+        }
+        simulation = new Simulation(args.get(0), parameters);
+
         JSONObject simulation_output = simulation.run();
-        dump_csv();
         simulation_output.put("run time", (System.currentTimeMillis() - started) / 1000.0);
         simulation_output.put("hash", simulation.getSha());
         System.out.print(simulation_output);
     }
-
-    private static void delete_database()
-    {
-        try
-        {
-            Runtime.getRuntime().exec("rm lmdss.sqlite3");
-        } catch (Exception ee)
-        {
-            System.out.println("Cannot run batch...");
-        }
-    }
-
-    private static void dump_csv()
-    {/*
-        try
-        {
-            Runtime.getRuntime().exec("cmd /c start DumpCSV.bat");
-        } catch (IOException e)
-        {
-            try
-            {
-                Runtime.getRuntime().exec("sh /home/taghawi/Dropbox/workspace/saudifirms/dump.sh");
-            }
-            catch (Exception ee)
-            {
-                System.out.println("Cannot run batch...");
-            }
-        }*/
-    }
 }
+
