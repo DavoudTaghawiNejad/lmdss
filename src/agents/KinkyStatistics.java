c24246000016a189fee8060e16c5d41bb58c40c0
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-04-14 18:04:41 -0400

    returns stats for firm level changes

--- src/agents/KinkyStatistics.java
+++ src/agents/KinkyStatistics.java
@@ -13,6 +13,8 @@ public class KinkyStatistics
 
     // distance bin distance_to_cut_off, sum of change of saudis
     HashMap<Integer, Integer> bin_change_saudis = new HashMap<Integer, Integer>();
+    HashMap<Integer, Integer> bin_change_expats = new HashMap<Integer, Integer>();
+    HashMap<Integer, Double> bin_change_Sauditization = new HashMap<Integer, Double>();
     // distance bin distance_to_cut_off, count
     HashMap<Integer, Integer> bin_count = new HashMap<Integer, Integer>();
 
@@ -70,22 +72,55 @@ public class KinkyStatistics
         {
             bin_count.put(firm.getDistance_to_cut_off(), 0);
             bin_change_saudis.put(firm.getDistance_to_cut_off(), 0);
+            bin_change_expats.put(firm.getDistance_to_cut_off(), 0);
+            bin_change_Sauditization.put(firm.getDistance_to_cut_off(), 0.0);
         }
         for (Firm firm: firms)
         {
             int delta_saudis = firm.getBefore_saudis() - firm.staff.getSaudis();
+            int delta_expats = firm.getBefore_expats() - firm.staff.getExpats();
+
+            double b_sauditization = 0;
+            double a_sauditization = 0;
+            try {
+                b_sauditization = firm.getBefore_saudis() / (firm.getBefore_saudis() + firm.getBefore_expats());
+                a_sauditization = firm.staff.getSaudis() / (firm.staff.getSaudis()+ firm.staff.getExpats());
+            } catch (Exception e) {
+            }
+
+
             int distance_to_cut_off = firm.getDistance_to_cut_off();
             bin_change_saudis.put(distance_to_cut_off, bin_change_saudis.get(distance_to_cut_off) + delta_saudis);
+            bin_change_expats.put(distance_to_cut_off, bin_change_expats.get(distance_to_cut_off) + delta_expats);
+            bin_change_Sauditization.put(distance_to_cut_off, bin_change_Sauditization.get(distance_to_cut_off) + b_sauditization - a_sauditization);
             bin_count.put(distance_to_cut_off, bin_count.get(distance_to_cut_off) + 1);
         }
     }
 
-    public JSONObject json()
+    public JSONObject saudis()
     {
         JSONObject nitaqat_calibration_statistics = new JSONObject();
         Set<Integer> keys = bin_change_saudis.keySet();
         for (Integer  key: keys) {
             nitaqat_calibration_statistics.put(key, bin_change_saudis.get(key) / bin_count.get(key));
+        }
+        return nitaqat_calibration_statistics;
+    }
+    public JSONObject expats()
+    {
+        JSONObject nitaqat_calibration_statistics = new JSONObject();
+        Set<Integer> keys = bin_change_expats.keySet();
+        for (Integer  key: keys) {
+            nitaqat_calibration_statistics.put(key, bin_change_expats.get(key) / bin_count.get(key));
+        }
+        return nitaqat_calibration_statistics;
+    }
+    public JSONObject sauditization()
+    {
+        JSONObject nitaqat_calibration_statistics = new JSONObject();
+        Set<Integer> keys = bin_change_Sauditization.keySet();
+        for (Integer  key: keys) {
+            nitaqat_calibration_statistics.put(key, bin_change_Sauditization.get(key) / bin_count.get(key));
         }
         return nitaqat_calibration_statistics;
     }
